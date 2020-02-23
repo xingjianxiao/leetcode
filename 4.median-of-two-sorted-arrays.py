93e10ed0209bcb1d4e2bc8910d5fd63cbe470487
xingjianxiao <vincent.xiao@mail.utoronto.ca>
2020-02-23 09:32:22 -0500

    python solution for #4

---
+++ 4.median-of-two-sorted-arrays.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=4 lang=python
+#
+# [4] Median of Two Sorted Arrays
+#
+
+# @lc code=start
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        
+# @lc code=end
+
